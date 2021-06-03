a1cc5dc4cbf118986ccbcab050f9706d48725aaa
KishoreColan <kishorekumar.m@colanonline.com>
2021-06-03 12:48:55 +0530

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,7 @@
                     {{ __('Dashboard') }}
                     <a href="{{ url('/about') }}" class="text-sm text-gray-700 underline float-right">About us</a>
                 </div>
+                <h1>Testing Changes</h1>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
